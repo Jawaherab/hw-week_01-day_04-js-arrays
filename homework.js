d5ef03b0d64614cf9bd94db0593aef7458f35060
JawaherAb <jawaheralabdullah0@gmail.com>
2019-04-25 02:27:09 +0300

    hw-week_01-day_04-js-arrays

---
+++ homework.js
@@ -0,0 +1,91 @@
+
+// question 1
+const summation = function(num){
+    let s=0;
+    for (let n =1 ; n => 1;n++){
+       
+        s += n;
+    
+    }
+    
+    return s ;
+    }
+
+
+// question 2
+
+
+// question 3
+
+
+
+
+
+
+
+// question 4
+
+function reverse(str){ 
+    return str.split("").reverse().join("");   
+ }
+
+ reverse(str);
+
+
+
+ // question 5
+
+ function addDashes (s){
+ 
+    return s.join("-");
+   }
+
+
+// question 6
+
+
+
+
+// question 7
+
+const wordsWithA = function(word){
+    let array = [];
+    for(let i = 0; i < word.length; i++){
+if (word[i] === "a"){
+                array.push(word[i])
+         
+            }
+    return word;
+}
+}
+// question 8
+
+
+
+// question 9
+
+const longestWord = function(str) {
+    let word= str.split(' ');
+    let longest=0;
+    let  a="";
+   for(let i = 0; i <= word.length ; i++){
+     if(word.length > longest){
+     longest= word.length;
+      a = word[i];
+      }
+   }
+   return a;
+ }
+
+// question 10
+
+
+
+
+
+
+
+
+
+
+
